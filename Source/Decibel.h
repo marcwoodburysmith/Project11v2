6378f49a1e467eb9e7c9471dd9221fbac701838a
marcwoodburysmith <marc.woodbury-smith@newcastle.ac.uk>
2024-08-11 07:57:04 +0100

    Step 7 - Created Decibel class

---
+++ Source/Decibel.h
@@ -0,0 +1,102 @@
+/*
+  ==============================================================================
+
+    Decibel.h
+    Created: 11 Aug 2024 7:48:58am
+    Author:  Marc Woodbury-Smith
+
+  ==============================================================================
+*/
+
+#pragma once
+#include <JuceHeader.h>
+
+template <typename FloatType>
+class Decibel {
+
+public:
+    Decibel() : decibels{0.0}
+    {
+    }
+    
+    explicit Decibel(FloatType dB) : decibels{dB}
+    {
+    }
+    
+    FloatType getDb() const
+    {
+        return decibels;
+    }
+    
+    FloatType getGain() const
+    {
+        return juce::Decibels::decibelsToGain(decibels);
+    }
+    
+    void setGain(FloatType g)
+    {
+        decibels = juce::Decibels::gainToDecibels(g);
+    }
+    
+    void setDb(FloatType db)
+    {
+        decibels = db;
+    }
+    
+    Decibel& operator+=(const Decibel& rhs)
+    {
+        decibels += rhs.decibels;
+        return *this;
+    }
+    
+    Decibel& operator-=(const Decibel& rhs)
+    {
+        decibels -= rhs.decibels;
+        return *this;
+    }
+    
+    friend Decibel operator+(Decibel lhs, const Decibel& rhs)
+    {
+        lhs += rhs;
+        return lhs;
+    }
+    
+    friend Decibel operator-(Decibel lhs, const Decibel& rhs)
+    {
+        lhs -= rhs;
+        return lhs;
+    }
+    
+    friend bool operator< (const Decibel& lhs, const Decibel& rhs)
+    {
+        return lhs.decibels < rhs.decibels;
+    }
+    
+    friend bool operator> (const Decibel& lhs, const Decibel& rhs)
+    {
+        return rhs < lhs;
+    }
+    
+    friend bool operator<=(const Decibel& lhs, const Decibel& rhs)
+    {
+        return !(lhs > rhs);
+    }
+    
+    friend bool operator>=(const Decibel& lhs, const Decibel& rhs)
+    {
+        return !(lhs < rhs);
+    }
+    
+    friend bool operator==(const Decibel& lhs, const Decibel& rhs)
+    {
+        return lhs.decibels == rhs.decibels;
+    }
+    
+    friend bool operator!=(const Decibel& lhs, const Decibel& rhs)
+    {
+        return !(lhs == rhs);
+    }
+    
+private:
+    FloatType decibels;
+};
